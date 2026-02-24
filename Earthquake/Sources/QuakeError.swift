1670815b88fd349495d3eb8e68ffab5c9763188c
tomchoi <qjatn0545@naver.com>
2026-02-24 19:42:28 +0900

    feat: 지진 데이터 모델 `Quake`와 관련 오류 처리 `QuakeError`를 추가합니다.

---
+++ Earthquake/Sources/QuakeError.swift
@@ -0,0 +1,24 @@
+//
+//  QuakeError.swift
+//  Earthquake
+//
+//  Created by Tom Choi on 2/24/26.
+//
+
+import Foundation
+
+enum QuakeError: Error {
+    case missingData
+}
+
+extension QuakeError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .missingData:
+            return NSLocalizedString(
+                "Found and will discard a quake missing a valid code, magnitude, place, or time.",
+                comment: ""
+            )
+        }
+    }
+}
