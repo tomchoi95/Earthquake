25216e0fad94f4dc262a62846e86993a0c2b45e6
tomchoi <qjatn0545@naver.com>
2026-02-24 20:05:08 +0900

    feat: JSONDecoder에 날짜 디코딩 전략을 설정하고 지진 발생 시간 디코딩을 검증하는 테스트를 추가

--- Earthquake/Tests/EarthquakesTests.swift
+++ Earthquake/Tests/EarthquakesTests.swift
@@ -14,7 +14,12 @@ struct EarthquakesTests {
     @Test
     func geoJSONDecoderDecodesQuake() throws {
         let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
         let quake = try decoder.decode(Quake.self, from: testFeature_nc73649170)
         #expect(quake.code == "73649170")
+
+        let expectedSeconds = TimeInterval(1_636_129_710_550 / 1000.0)
+        let expectedTime = Date(timeIntervalSince1970: expectedSeconds)
+        #expect(quake.time == expectedTime)
     }
 }
