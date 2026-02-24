3178091c3c17d308cc0f9080735f74a77921ac2b
tomchoi <qjatn0545@naver.com>
2026-02-24 22:02:32 +0900

    feat: 지진 위치 데이터 모델 QuakeLocation을 추가했습니다.

---
+++ Earthquake/Sources/QuakeLocation.swift
@@ -0,0 +1,56 @@
+//
+//  QuakeLocation.swift
+//  Earthquake
+//
+//  Created by Tom Choi on 2/24/26.
+//
+
+import Foundation
+
+struct QuakeLocation: Decodable {
+    var latitude: Double { properties.products.origin.first!.properties.latitude }
+    var longitude: Double { properties.products.origin.first!.properties.longitude }
+    private var properties: RootProperties
+}
+
+extension QuakeLocation {
+    struct RootProperties: Decodable {
+        var products: Products
+    }
+
+    struct Products: Decodable {
+        var origin: [Origin]
+    }
+
+    struct Origin: Decodable {
+        var properties: OriginProperties
+    }
+
+    struct OriginProperties {
+        var latitude: Double
+        var longitude: Double
+    }
+}
+
+extension QuakeLocation.OriginProperties: Decodable {
+    private enum OriginPropertiesCodingKeys: String, CodingKey {
+        case latitude
+        case longitude
+    }
+
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: OriginPropertiesCodingKeys.self)
+        let longitude = try container.decode(String.self, forKey: .longitude)
+        let latitude = try container.decode(String.self, forKey: .latitude)
+
+        guard
+            let longitude = Double(longitude),
+            let latitude = Double(latitude)
+        else {
+            throw QuakeError.missingData
+        }
+
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
